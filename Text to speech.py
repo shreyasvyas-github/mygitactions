d1b3421ae93895b525fd0e8c35f899420d30a950
shreyasvyas <49509845+shreyasvyas@users.noreply.github.com>
2021-06-09 09:13:15 +0530

    Update Text to speech.py

--- Text to speech.py
+++ Text to speech.py
@@ -1,8 +1,8 @@
 from gtts import gTTS
 
 import os
-my_text = "Welcome to Our Inventory,Kindly Purchase your needs available"
 
+my_text = "Welcome to Our Inventory,Kindly Purchase your needs available"
 
 language = "en"
 output = gTTS(text=my_text, lang=language, slow=False)
