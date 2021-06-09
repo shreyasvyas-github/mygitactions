e447b4633f646e46d02d03caa3f70591064cbc68
shreyasvyas <49509845+shreyasvyas@users.noreply.github.com>
2021-06-09 08:32:55 +0530

    Create Text to speech.py

---
+++ Text to speech.py
@@ -0,0 +1,10 @@
+from gtts import gTTS
+import os
+my_text='Welcome to Our Inventory,Kindly Purchase your needs available'
+
+language= 'en'
+output=gTTS(text=my_text, lang=language ,slow= False)
+
+output.save('purchase.mp3')
+
+os.system('start purchase.mp3')
