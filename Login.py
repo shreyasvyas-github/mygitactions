b1aa330bb760614e5b0d7580c8b15696648ce5f2
shreyasvyas <49509845+shreyasvyas@users.noreply.github.com>
2021-06-09 09:33:55 +0530

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,42 +1,54 @@
 from tkinter import *
-from PIL import Image,ImageTk,ImageDraw
+from PIL import Image, ImageTk,ImageDraw
 from datetime import *
 import time
 from math import *
 from tkinter import messagebox
 from playsound import playsound
 import pymysql
+
+
 class Login_window:
-    def __init__(self,root):
+    def __init__(self, root):
         self.root = root
-        self.root.title("                                                                                                                                                                       Inventory Login")
+        self.root.title(" 
+                        Inventory Login
+                        ")
         self.root.geometry("1350x700+0+0")
         self.root.config(bg="#021e2f")
 
         # ===================Background Colours=========================================================
-        left_lbl=Label(self.root,bg="#08A3D2",bd=0)
-        left_lbl.place(x=0,y=0, relheight=1,width=600)
+        left_lbl=Label(self.root, bg="#08A3D2", bd=0)
+        left_lbl.place(x=0, y=0, relheight=1, width=600)
 
         right_lbl = Label(self.root, bg="#031F3C", bd=0)
         right_lbl.place(x=600, y=0, relheight=1, relwidth=1)
 
         # ===================Frames=========================================================
-        login_frame = Frame(self.root,bg = "white")
-        login_frame.place(x=250,y=100,width=800,height=500)
+        login_frame = Frame(self.root, bg="white")
+        login_frame.place(x=250, y=100, width=800, height=500)
 
-        title = Label(login_frame,text="LOGIN HERE",font=("Times New Roman",30,"bold"),bg="white",fg="#08A3D2").place(x=250,y=50)
+        title = Label(login_frame
+                      ,text="LOGIN HERE"
+                      ,font=("Times New Roman",30,"bold")
+                      ,bg="white",fg="#08A3D2")
+                        .place(x=250,y=50)
 
-        email = Label(login_frame, text="EMAIL ADDRESS", font=("Times New Roman", 18, "bold"), bg="white",fg="gray").place(x=250, y=150)
-        self.txt_email = Entry(login_frame,font=("Times New Roman", 15),bg="lightgray")
-        self.txt_email.place(x=250, y=180,width=350,height=35)
+        email = Label(login_frame
+                      , text="EMAIL ADDRESS", 
+                      font=("Times New Roman", 18, "bold")
+                      , bg="white",fg="gray")
+                        .place(x=250, y=150)
+        self.txt_email = Entry(login_frame, font=("Times New Roman", 15), bg="lightgray")
+        self.txt_email.place(x=250, y=180, width=350, height=35)
 
         pass_ = Label(login_frame, text="PASSWORD", font=("Times New Roman", 18, "bold"), bg="white",fg="gray").place(x=250, y=250)
         self.txt_pass_ = Entry(login_frame, font=("Times New Roman", 15), bg="lightgray")
         self.txt_pass_.place(x=250, y=280, width=350, height=35)
 
-        btn_reg=Button(login_frame,cursor = "hand2",command = self.register_window, text = "Register new Account?", font=("times new roman",14), bg = "white",bd=0,fg="#B00857").place(x=250,y=320)
+        btn_reg=Button(login_frame, cursor="hand2", command = self.register_window, text = "Register new Account?", font=("times new roman",14), bg = "white",bd=0, fg="#B00857").place(x=250,y=320)
 
-        btn_login = Button(login_frame, text = "Login", command = self.login, font = ("times new roman", 20,"bold"), fg ="white", bg ="#B00857",cursor = "hand2").place(x = 250, y = 380, width = 180, height = 40)
+        btn_login = Button(login_frame, text = "Login", command = self.login, font=("times new roman", 20,"bold"), fg="white", bg="#B00857",cursor="hand2").place(x=250, y=380, width = 180, height = 40)
 
 
         playsound('Login.mp3')
